0e33b0f9d686c9f8d7290d59f75d99f050d76e17
Ashutosh Tripathy <ashutoshtripathy702@gmail.com>
2022-06-26 23:46:15 +0530

    email update

--- backend/middlewares/sendEmail.js
+++ backend/middlewares/sendEmail.js
@@ -5,8 +5,8 @@ exports.sendEmail = async (options) => {
     host: "smtp.mailtrap.io",
     port: 2525,
     auth: {
-      user: "211d4679e3d79c",
-      pass: "9e2b7d43009e26",
+      user: "f01db27c7211f0",
+      pass: "1f6200e208e32a",
     },
   });
 
